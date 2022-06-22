12742780b51cdac4bf32986dac4f040289726cad
Ruben <ruben.bliersbach@gmail.com>
2022-06-22 16:03:29 +0200

    Sim running, combining mesh from sim and existing texture not working yet

--- lab/experiment1.py
+++ lab/experiment1.py
@@ -3,39 +3,81 @@ import arcsim
 import sys
 import json
 
+from pytorch3d.io import load_obj
+from pytorch3d.renderer import TexturesUV
+from pytorch3d.structures import Meshes
+
 sys.path.append('../pysim')
 
 def reset_sim():
 	arcsim.init_physics('curconf.json','',False)
-	g = sim.gravity
-	g.requires_grad = True
-	return g
+	mat = sim.cloths[0].materials[0]
+	density = mat.densityori
+	stretch = mat.stretchingori
+	bend = mat.bendingori
+	return density, stretch, bend
 
 def run_sim():
-	for step in range(49):
-		arcsim.sim_step()
+	arcsim.sim_step()
+
 
 def save_config(config, file):
 	with open(file,'w') as f:
 		json.dump(config, f)
 
 
-
 def to_mesh(vertices, faces):
 	pass
 
 
-with open("./real/S1/sim_conf.json") as file:
+with open("./real/S1/sim_conf.json", 'r') as file:
     config = json.load(file)
+
+matfile = config['cloths'][0]['materials'][0]['data']
+
+with open(matfile,'r') as f:
+	matconfig = json.load(f)
+
+save_config(matconfig, 'matconfig.json')
+config['cloths'][0]['materials'][0]['data'] = 'matconfig.json'
 save_config(config, 'curconf.json')
 
 
-torch.set_num_threads(8)
 sim=arcsim.get_sim()
 
-g = reset_sim()
-#run_sim()
+density, stretch, bend = reset_sim()
+run_sim()
 
-#mat = sim.cloths[0].materials[0]
+verts_tensor = torch.tensor([])
+for node in sim.cloths[0].mesh.nodes:
+	verts_tensor = torch.cat((verts_tensor, node.x))
 
-#print(mat)
+
+
+faces_tensor = torch.tensor([], dtype=torch.int)
+
+for face in sim.cloths[0].mesh.faces:
+	faces_tensor = torch.cat((faces_tensor, torch.tensor(
+		[face.vertices[0].index, face.vertices[1].index, face.vertices[2].index])))
+
+
+verts_tensor = verts_tensor.view(-1,3)
+faces_tensor = faces_tensor.view(-1,3)
+
+verts, faces, aux = load_obj("real/S1/templates/template_mesh_final_textured.obj")
+
+verts_uvs = aux.verts_uvs[None, ...]  # (1, V, 2)
+faces_uvs = faces.textures_idx[None, ...]  # (1, F, 3)
+tex_maps = aux.texture_images
+
+# tex_maps is a dictionary of {material name: texture image}.
+# Take the first image:
+texture_image = list(tex_maps.values())[0]
+texture_image = texture_image[None, ...]  # (1, H, W, 3)
+
+# Create a textures object
+tex = TexturesUV(verts_uvs=verts_uvs, faces_uvs=faces_uvs, maps=texture_image)
+
+print(tex)
+
+meshes = Meshes(verts=[verts_tensor], faces=[faces_tensor], textures=tex)
